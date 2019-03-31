06243cee0941542fa7e8ae4ef464e5766ffd6da1
Mauricio Laine <laine.mau@gmail.com>
2019-03-30 21:03:25 -0700

    add a live data functionality to show the name of the user in a Toast

--- app/src/main/java/com/laine/mauro/users/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/users/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.laine.mauro.users
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.laine.mauro.users.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +19,15 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.user = model.getNextUser()
 
+        val nameObserver = Observer<String> { test ->
+            Toast.makeText(this, test, Toast.LENGTH_SHORT).show()
+        }
+        model.test.observe(this, nameObserver)
+
         binding.nextButton.setOnClickListener {
-            binding.user = model.getNextUser()
+            val user = model.getNextUser()
+            binding.user = user
+            model.setNewValue(user.name)
         }
     }
 }
--- app/src/main/java/com/laine/mauro/users/UserViewModel.kt
+++ app/src/main/java/com/laine/mauro/users/UserViewModel.kt
@@ -1,5 +1,6 @@
 package com.laine.mauro.users
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 class UserViewModel : ViewModel() {
@@ -12,9 +13,17 @@ class UserViewModel : ViewModel() {
         User("Federico Mena", "Mexico City", "Novell")
     )
 
+    val test: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
     fun getNextUser(): User {
         position++
         return users[getNextPosition()]
+    }
+
+    fun setNewValue(newTest: String) {
+        test.value = newTest
     }
 
     private fun getNextPosition(): Int {
